ae54775b9d875241139fe166813fa2b5d6d1f67e
patricianunes <pcampos@scentregroup.com>
2019-10-24 22:11:51 +1100

    My first Swift App - Todo List using Table View Controller

---
+++ Night Porter Table View/Task.swift
@@ -0,0 +1,13 @@
+
+import Foundation
+
+enum TaskType {
+    case daily, weekly, monthly
+}
+
+struct Task {
+    var name : String
+    var type : TaskType
+    var completed : Bool
+    var lastCompleted : NSDate?
+}
--- Night Porter Table View/ViewController.swift
+++ Night Porter Table View/ViewController.swift
@@ -6,10 +6,182 @@
 //  Copyright © 2019 Patricia Campos Nunes. All rights reserved.
 //
 
+
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var taskTableView: UITableView!
+    
+    //Create task arrays
+    
+    var dailyTasks = [
+        Task(name: "Check all windows", type: .daily, completed: false, lastCompleted: nil),
+        Task(name: "Check all doors", type: .daily, completed: false, lastCompleted: nil),
+        Task(name: "Is the boiler fueled?", type: .daily, completed: false, lastCompleted: nil),
+        Task(name: "Check the mailbox", type: .daily, completed: false, lastCompleted: nil),
+        Task(name: "Empty trash containers", type: .daily, completed: false, lastCompleted: nil),
+        Task(name: "If freezing, check water pipes", type: .daily, completed: false, lastCompleted: nil),
+        Task(name: "Document \"strange and unusual\" occurences", type: .daily, completed: false, lastCompleted: nil),
+    ]
+    
+    var weeklyTasks = [
+        Task(name: "Check iside all cabins", type: .weekly, completed: false, lastCompleted: nil),
+        Task(name: "Flush all lavatories in cabins", type: .weekly, completed: false, lastCompleted: nil),
+        Task(name: "Walk the perimenter of property", type: .weekly, completed: false, lastCompleted: nil),
+        ]
+    
+    var monthlyTasks = [
+        Task(name: "Test security alar", type: .monthly, completed: false, lastCompleted: nil),
+        Task(name: "Test motion detectors", type: .monthly, completed: false, lastCompleted: nil),
+        Task(name: "Test smoke alarms", type: .monthly, completed: true, lastCompleted: nil),
+        ]
+    
+    @IBAction func taggleDarkMode(_ sender: Any) {
+        let mySwitch = sender as! UISwitch
+        
+        if mySwitch.isOn {
+            view.backgroundColor = UIColor.darkGray
+        } else {
+            view.backgroundColor = UIColor.white
+        }
+    }
+    
+    @IBAction func resetList(_ sender: Any) {
+        
+        let confirm = UIAlertController(title: "Are You Sure?", message: "Really reset the list?", preferredStyle: .alert)
+        
+        let yesAction = UIAlertAction(title: "Yes", style: .destructive) {
+            action in
+            
+            for i in 0..<self.dailyTasks.count {
+                self.dailyTasks[i].completed = false
+            }
+            
+            for i in 0..<self.weeklyTasks.count {
+                self.weeklyTasks[i].completed = false
+            }
+            
+            for i in 0..<self.monthlyTasks.count {
+                self.monthlyTasks[i].completed = false
+            }
+            
+            self.taskTableView.reloadData()
+        }
+        
+        let noAction = UIAlertAction(title: "No", style: .cancel) {
+            action in
+            print("No action")
+        }
+        
+        //Add action to alert controller
+        confirm.addAction(yesAction)
+        confirm.addAction(noAction)
+        
+        //present or show it
+        present(confirm, animated: true, completion: nil)
+    }
+    
+    
+    //Table View Delegate Methods
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("You selected row \(indexPath.row) in section \(indexPath.section)")
+    }
+    
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let completeAction = UIContextualAction(style: .normal, title: "Complete") { (action: UIContextualAction, sourceview:UIView, actionPerformed:(Bool) -> Void) in
+            
+            //find the right object and set ir to completed
+            switch indexPath.section {
+            case 0:
+                self.dailyTasks[indexPath.row].completed = true
+            case 1:
+                self.weeklyTasks[indexPath.row].completed = true
+            case 2:
+                self.monthlyTasks[indexPath.row].completed = true
+            default:
+                break
+            }
+            
+            tableView.reloadData()
+            
+            actionPerformed(true)
+            
+        }
+        
+        return UISwipeActionsConfiguration(actions: [completeAction])
+    }
+    
+    //Table View Data Source Methods
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        tableView.backgroundColor = UIColor.clear
+        
+        return 3
+    }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+        case 0:
+            return dailyTasks.count
+        case 1:
+            return weeklyTasks.count
+        case 2:
+            return monthlyTasks.count
+        default:
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell()
+        
+        //To hold the current task
+        var currentTask: Task!
+        
+        switch indexPath.section {
+        case 0:
+            currentTask = dailyTasks[indexPath.row]
+        case 1:
+            currentTask = weeklyTasks[indexPath.row]
+        case 2:
+            currentTask = monthlyTasks[indexPath.row]
+        default:
+            break
+        }
+        
+        //use the name property to set the value of the cell
+        cell.textLabel!.text = currentTask.name
+        
+        if currentTask.completed {
+            cell.textLabel?.textColor = UIColor.lightGray
+            cell.accessoryType = .checkmark
+        } else {
+            cell.textLabel?.textColor = UIColor.black
+            cell.accessoryType = .none
+        }
+        
+        //clear out the background
+        cell.backgroundColor = UIColor.clear
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section {
+        case 0:
+            return "Daily Tasks"
+        case 1:
+            return "Weekly Tasks"
+        case 2:
+            return "Monthly Tasks"
+        default:
+            return nil
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
